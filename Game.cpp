9b18c1ca0e2389d5e2e8bb324bfc818d4211eac4
liorZahavi <lior.zahavi@stu.shenkar.ac.il>
2026-01-07 14:48:01 +0200

    build main
    coding the main class and it's functionality, and open class pages for HumanPlayer.cpp,HumanPlayer.h,AiPlayer.h,AiPlayer.cpp,Grid.h,Grid.cpp

--- Game.cpp
+++ Game.cpp
@@ -1,17 +1,38 @@
 #include "Game.h"
-
+#include "Player.h"
+#include <iostream>
 namespace BattleShips
 {
-    Game::Game(Player *p1, Player *p2)
+    Game::Game(Player *p1, Player *p2) : player1(p1), player2(p2)
     {
+        if (!player1 || !player2)
+            throw std::invalid_argument("Game received null player");
     }
     void Game::setup()
     {
+        player1->placeAllShips();
+        player2->placeAllShips();
     }
     void Game::start()
     {
+        while (!isGameOver())
+        {
+            player1->makeMove(player2);
+            if (player2->allShipsSunk())
+            {
+                std::cout << "The winner is:" << player1->getName() << std::endl;
+                return;
+            }
+            player2->makeMove(player1);
+            if (player1->allShipsSunk())
+            {
+                std::cout << "The winner is: " << player2->getName() << std::endl;
+                return;
+            }
+        }
     }
     bool Game::isGameOver() const
     {
+        return (player1->allShipsSunk() || player2->allShipsSunk());
     }
 }
---
+++ Main.cpp
@@ -0,0 +1,18 @@
+#include "Game.h"
+#include "Player.h"
+#include <cstdlib>
+#include <ctime>
+
+int main()
+{
+    std::srand(static_cast<unsigned>(std::time(nullptr)));
+
+    BattleShips::Player *HumanPlayer = new BattleShips::HumanPlayer("Human");
+    BattleShips::Player *AiPlayer = new BattleShips::AiPlayer("Computer");
+    BattleShips::Game game(HumanPlayer, AiPlayer);
+    game.setup();
+    game.start();
+    delete HumanPlayer;
+    delete AiPlayer;
+    return 0;
+}
